ad1bfeff19c81023a41526d6421b0a5d85146882
Benjamin Che <BenjaminAChe@users.noreply.github.com>
2015-02-28 10:51:33 -0500

    Division functionality

--- src/n2k/Main.java
+++ src/n2k/Main.java
@@ -11,30 +11,48 @@ import java.util.Random;
  * employing addition, subtraction, multiplication, division, roots, or exponents, 
  * in such a way that the result is a number from one to thirty-six. 
  * All three numbers must be used once, but cannot be used more than once. 
- * @author CHEBA1
+ * @author Benjamin Che
  */
 public class Main {
 	
 	private static final int[][] permutations = {{0,1,2}, {0,2,1}, {1,0,2}, {1,2,0}, {2,0,1}, {2,1,0}};
 
+	/** SETTINGS */
+	private static final int MATCH_LIMIT = 102;
+	
+	private static final int[] rolls2 = {6, 6, 2};
+	
+	private static boolean randomRolls = false;
+	
+	private static String[] outcomes = new String[MATCH_LIMIT];
+	
+	/** END SETTINGS */
+	
 	public static void main(String[] args) {
-		int[] rolls = rollThreeDice();
+		int[] rolls = new int[3];
+		if(randomRolls) {
+			for(int o = 0; o < 3; o++) {
+				rolls[o] = rollThreeDice(1, 6);
+			}
+		} else {
+			rolls = rolls2;
+		}
 		for(int i = 0; i < rolls.length; i++) {
 			System.out.println("Roll " + (i + 1) + " - " + rolls[i]);
 		}
 		fillSlots();
-		System.out.println("Filled in 36 slots from 1-36");
+		System.out.println();
 		/**
 		 * Start by knocking out numbers that we can match with simple operation combinations
 		 */
 		exitFlag:
-		for(int j = 36; j > 0; j--) {
+		for(int j = MATCH_LIMIT; j > 0; j--) { 
 			int match = j;
 			for(int k = 0; k < permutations.length; k++) {
 				for(Value l : doSomething(rolls[permutations[k][0]], rolls[permutations[k][1]])) {
 					for(Value m : doSomething(l.toInt(), rolls[permutations[k][2]])) {
 						if(m.toInt() == match) {
-							System.out.println("Match! " + match + " is the result of " + rolls[permutations[k][0]] + " " + l.getOperation().getName() + " " + rolls[permutations[k][1]] + " " + m.getOperation().getName() + " " + rolls[permutations[k][2]]);
+							outcomes[match - 1] = match + " is the result of " + rolls[permutations[k][0]] + l.getOperation().getName() + " " + rolls[permutations[k][1]] + " " + m.getOperation().getName() + " " + rolls[permutations[k][2]];
 							numberSlots.remove(j - 1);
 							continue exitFlag;
 						}
@@ -57,14 +75,17 @@ public class Main {
 							String extra1 = (l.getExponent() != 1) ? (" to the " + l.getExponent() + " power ") : "";
 							String extra2 = (l.getExponent2() != 1) ? (" to the " + l.getExponent2() + " power ") : "";
 							String extra3 = (m.getExponent2() != 1) ? (" to the " + m.getExponent2() + " power ") : "";
-							System.out.println("Match! " + match + " is the result of " + rollsValue[permutations[k][0]].toInt() + extra1 + " " + l.getOperation().getName() + " " + rollsValue[permutations[k][1]].toInt() + extra2 + " " + m.getOperation().getName() + " " + rollsValue[permutations[k][2]].toInt() + extra3);
+							outcomes[match - 1] = match + " is the result of " + rollsValue[permutations[k][0]].toInt() + extra1 + " " + l.getOperation().getName() + " " + rollsValue[permutations[k][1]].toInt() + extra2 + " " + m.getOperation().getName() + " " + rollsValue[permutations[k][2]].toInt() + " " + extra3;
 							continue exitFlag2;
 						}
 					}
 				}
 			}
-			System.out.println("Missed this - " + i);
 		}
+		for(String s : outcomes) {
+			System.out.println(s);
+		}
+
 	}
 	
 	private static List<Value> doSomething(int a, int b) {
@@ -76,8 +97,8 @@ public class Main {
 		return toReturn;
 	}
 	
-	private static final double[] exponentOperations = {0.5, 0.25, 0.333, 0, 2, 3, 4, 5, 6};
-	
+	private static final double[] exponentOperations = {0,1,2,3,0.5,1.5,2.5,3.5,0.25,0.75,1.25,1.75,0.333,0.666,1.333,1.666,2.333,4,5,6,7,8};
+
 	private static List<Value> doSomething(Value a, Value b, boolean ignoreFirst) {
 		List<Value> toReturn = new ArrayList<Value>();
 		/** Exponent manipulation for addition */
@@ -149,8 +170,30 @@ public class Main {
 				}
 			}
 		}
-		/** TODO: Exponent manipulation for division */
-		if((a.toInt() % b.toInt()) == 0) { toReturn.add(new Value(a.toInt()/b.toInt(), Operation.DIVIDE)); }
+		/** Exponent manipulation for division */
+		for(double i : exponentOperations) {
+			double exponentResultA = Math.pow(a.toInt(), i);
+			if(exponentResultA % 1 == 0  && !ignoreFirst) {
+				for(double j : exponentOperations) {
+					double exponentResultB = Math.pow(b.toInt(), j);
+					if(exponentResultB % 1 == 0) {
+						toReturn.add(new Value((int)exponentResultA / (int)exponentResultB, Operation.DIVIDE, i, j));
+					} else {
+						toReturn.add(new Value((int)exponentResultA / b.toInt(), Operation.DIVIDE, i, 1));
+					}
+				}
+			} else {
+				for(double j : exponentOperations) {
+					double exponentResultB = Math.pow(b.toInt(), j);
+					if(exponentResultB % 1 == 0) {
+						toReturn.add(new Value(a.toInt() / (int)exponentResultB, Operation.DIVIDE, 1, j));
+					} else {
+						toReturn.add(new Value(a.toInt() / b.toInt(), Operation.DIVIDE, 1, 1));
+					}
+				}
+			}
+		}
+		//if((a.toInt() % b.toInt()) == 0) { toReturn.add(new Value(a.toInt()/b.toInt(), Operation.DIVIDE)); }
 		return toReturn;
 	}
 	
@@ -162,22 +205,27 @@ public class Main {
 		return toReturn;
 	}
 	
-	private static int[] rollThreeDice() {
-		Random r = new Random();
-		int[] rolls = new int[3];
-		for(int i = 0; i < 3; i++) {
-			rolls[i] = (r.nextInt(6) % 6) + 1;
+		public static int rollThreeDice(int min, int max) {
+
+		    // NOTE: Usually this should be a field rather than a method
+		    // variable so that it is not re-seeded every call.
+		    Random rand = new Random();
+
+		    // nextInt is normally exclusive of the top value,
+		    // so add 1 to make it inclusive
+		    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		    return randomNum;
 		}
-		return rolls;
-	}
+
 	
 	private static void fillSlots() {
-		for(int i = 0; i < 36; i++) {
+		for(int i = 0; i < MATCH_LIMIT; i++) {
 			numberSlots.add(i + 1);
 		}
 	}
 	
-	private static List<Integer> numberSlots = new ArrayList<Integer>(36);
+	private static List<Integer> numberSlots = new ArrayList<Integer>(MATCH_LIMIT);
 	
 	
 	public enum Operation {
